495088dcd6f64156b17764f5a3657f84f9a3a2ad
eginoy <eginoy.oheso@gmail.com>
2020-11-10 22:51:00 +0900

    2.5

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,1 +1,15 @@
 console.log('Hello TypeScript!')
+
+let a = 1 + 2
+let b = a + 3
+let c ={
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
+//TypeError
+let d = "apple"
+
+//TypeScriptが推論できない型のコード
+let e = {} + 1
