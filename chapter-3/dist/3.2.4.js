f4bc47f4268bd08026eaafba1e706922cbb94ac0
eginoy <eginoy.oheso@gmail.com>
2020-11-12 22:50:58 +0900

    3.2.4 number

---
+++ chapter-3/dist/3.2.4.js
@@ -0,0 +1,11 @@
+"use strict";
+const chapter3_2_4 = () => {
+    let a = 1234; //number型
+    let b = Infinity * 0.10; //number型
+    const c = 5678; //5678型 (literal型)
+    let d = a < b; //boolean型
+    let e = 100; //number型
+    let f = 26.218; //26.218型 (literal型)
+    // let g: 26.218 = 10 //26.218型 (literal型) 26.218以外で初期化しようとしているのでerror
+};
+//# sourceMappingURL=3.2.4.js.map
