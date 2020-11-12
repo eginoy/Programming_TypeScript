007e659f7a03978d3cf57f0fbc667ecab69b95cf
eginoy <eginoy.oheso@gmail.com>
2020-11-12 22:39:55 +0900

    3.2.3 boolean

---
+++ chapter-3/dist/3.2.3.js
@@ -0,0 +1,10 @@
+"use strict";
+const chapter3_2_3 = () => {
+    let a = true; //boolean 型
+    var b = false; //boolean 型
+    const c = true; //constで宣言されていて再代入できないので true 型(literal型)
+    let d = true; //boolean 型
+    let e = true; //true 型(literal型)
+    // let f: true = false //true 型なのでfalseはエラー
+};
+//# sourceMappingURL=3.2.3.js.map
