fa03e2adfe2992d84c30e818bf499951a62cd3a3
eginoy <eginoy.oheso@gmail.com>
2020-11-17 18:06:50 +0900

    4.1.4 thisの型付け

---
+++ chapter-4/src/4.1.4.ts
@@ -0,0 +1,23 @@
+const chapter4_1_4 = () => {
+    let x = {
+        a() {
+            return this
+        }
+    }
+    x.a() //a()の本体内では、thisはオブジェクト
+    
+    let a = x.a
+    a() //a()の本体内ではthisはundefined
+
+    //thisがanyなのでerror
+    // function fancyDate(){
+    //     return `${this.getMonth() + 1}/${this.getDate()}/${this.getFullYear()}`
+    // }
+
+    //thisにアノテートすることもできる
+    function fancyDate(this: Date){
+        return `${this.getMonth() + 1}/${this.getDate()}/${this.getFullYear()}`
+    }
+    fancyDate.call(new Date)
+    // fancyDate.call() //error thisにアノテートしたおかげで、TypeScriptがthisをチェックしてくれる
+}
