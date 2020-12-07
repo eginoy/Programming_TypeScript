27529041637e159d3f016f4dbb8373776a946798
eginoy <eginoy.oheso@gmail.com>
2020-12-07 12:35:31 +0900

    5.5クラスは構造的に型付けされる

---
+++ chapter-5/src/5.5.ts
@@ -0,0 +1,39 @@
+const chapter5_5 = () => {
+    class Zebra {
+        trot(){
+            console.info('ok')
+        }
+    }
+
+    class Poodle{
+        trot(){
+            console.info('ok')
+        }
+    }
+
+    function ambleAround(animal: Zebra){
+        animal.trot()
+    }
+
+    let zebra = new Zebra
+    let poodle = new Poodle
+
+    ambleAround(zebra) //ok
+
+    //ambleAroundの引数はZebra型でアノテートされているが、TypeScriptはクラスを構造的に型付けするのでエラーにならない。
+    //名前での型付けでないので、構造が同じならおk
+    ambleAround(poodle) //ok 
+
+
+    class A{
+        private x = 1
+    }
+    class B extends A{}
+    class C extends B{}
+    function f(a: A){}
+
+    f(new A)
+    f(new B)
+    f({x: 1}) //error {x: 1}はAまたはBのインスタンスでないのでprivate フィールドに割り当て不可
+    f(new C)
+}
